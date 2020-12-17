fc862d9ddc60aee4667ebf2df6b6919850865863
kwon <wingseter@gmail.com>
2020-12-17 09:43:52 +0900

    유저 추가 기능 완성

--- bebone/bebone.cpp
+++ bebone/bebone.cpp
@@ -22,35 +22,79 @@ bebone::bebone(QWidget *parent)
     // 버튼 연결
     connect(ui.refreshButton, SIGNAL(clicked()), this, SLOT(refreshButtonClicked()));
     connect(ui.searchButton, SIGNAL(clicked()), this, SLOT(searchButtonClicked()));
+    connect(ui.submitButton, SIGNAL(clicked()), this, SLOT(inputButtonClicked()));
 }
 
 void bebone::refresh()
 {
-    // 전체 User 목록 출력
+    // 전체 User 목록 불러오기
     model = new QSqlQueryModel;
-    model->setQuery("SELECT * FROM Users", db);
+    model->setQuery("SELECT * FROM Visitors", db);
 
+    // 전체 User 출력
     ui.beboneTable->setModel(model);
     ui.beboneTable->show();
 }
 
 void bebone::searchButtonClicked()
 {
-    // 특정 id 사용자 출력
+    // 특정 사용자 검색
     QSqlQuery query;
-    query.prepare("EXEC dbo.user_search :userId");
-    query.bindValue(":userId", ui.idInput->text());
-    query.exec();
-    qDebug() << query.lastError();
+    if (ui.byId->isChecked()) {
+        query.prepare("EXEC dbo.find_visitor_id :userId");
+        query.bindValue(":userId", ui.idInput->text());
+        query.exec();
+    }
+    else if (ui.byName->isChecked()) {
+        query.prepare("EXEC dbo.find_visitor_by_name :name");
+        query.bindValue(":name", ui.idInput->text());
+        query.exec();
+    }
 
     query.next();
     ui.name->setText(query.value(1).toString());
     ui.address->setText(query.value(2).toString());
-    ui.email->setText(query.value(3).toString());
+    ui.phone->setText(query.value(3).toString());
+
     query.finish();
 }
 
 void bebone::refreshButtonClicked()
 {
+    refresh();
+}
+
+void bebone::inputButtonClicked()
+{
+    QString name = ui.nameInput->text() ;
+    QString location = ui.addInput->text();
+    QString phone = ui.phoneInput->text();
+
+
+    // 입력 가능 검사
+    if (!QString::compare(name, "", Qt::CaseInsensitive)) {
+        msgBox.setText("InputName.");
+        msgBox.exec();
+        return;
+    }
+    if (!QString::compare(location, "", Qt::CaseInsensitive)) {
+        msgBox.setText("Input Location.");
+        msgBox.exec();
+        return;
+    }
+    if (!QString::compare(phone, "", Qt::CaseInsensitive)) {
+        msgBox.setText("Input PhoneNumber.");
+        msgBox.exec();
+        return;
+    }
+
+    // 새로운 유저 입력
+    QSqlQuery query;
+    query.prepare("EXEC dbo.input_new_user :Name,:Location,:Phone");
+    query.bindValue(":Name", name);
+    query.bindValue(":Location", location);
+    query.bindValue(":Phone", phone);
+    query.exec();
+
     refresh();
 }
--- bebone/bebone.h
+++ bebone/bebone.h
@@ -4,8 +4,9 @@
 #include <QtSql/qsqldatabase.h>
 #include <QtSql/qsqltablemodel.h>
 #include <QtSql/qsqlquery.h>
-#include <qdebug.h>
 #include <QtSql/qsqlerror.h>
+#include <qmessagebox.h>
+#include <qdebug.h>
 #include "ui_bebone.h"
 #include "static.h"
 
@@ -13,18 +14,30 @@ class bebone : public QMainWindow
 {
     Q_OBJECT
 private:
+    // 데이터 베이스 변수
     QSqlDatabase db;
+    // 연결 완료 저장 변수
     bool connected;
+    // 메시지 박스
+    QMessageBox msgBox;
 
 public:
+    // 테이블 포틴터
     QSqlQueryModel* model;
+    // 생성자
     bebone(QWidget *parent = Q_NULLPTR);
 
 private:
+    // ui 정보 저장 변수
     Ui::beboneClass ui;
+    // 테이블 새로고침
     void refresh();
 
 public slots:
+    // 검색 버튼 클릭시 호출
     void searchButtonClicked();
+    // 새로고침 버튼 클릭시 호출
     void refreshButtonClicked();
+    // 명부 입력 버튼 클릭시 호출
+    void inputButtonClicked();
 };
